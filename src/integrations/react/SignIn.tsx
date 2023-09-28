35407730e3f56c9bd32d4d92b13bf565ba984c22
gioboa <giorgiob.boa@gmail.com>
2023-09-28 23:28:36 +0200

    linter 🧽

--- src/integrations/react/SignIn.tsx
+++ src/integrations/react/SignIn.tsx
@@ -1,52 +1,52 @@
 /* eslint-disable qwik/jsx-img */
 /** @jsxImportSource react */
 
-import { qwikify$ } from '@builder.io/qwik-react';
-import { Button, Container, TextField } from '@mui/material';
-import { useState } from 'react';
+import { qwikify$ } from "@builder.io/qwik-react";
+import { Button, Container, TextField } from "@mui/material";
+import { useState } from "react";
 
 export const SignIn = qwikify$(({ onSummit }: { onSummit: Function }) => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	return (
-		<Container component='main' maxWidth='xs'>
-			<div className='wrapper__form'>
-				<img className='logo' src='/images/logo.png' alt='logo' />
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className="wrapper__form">
+        <img className="logo" src="/images/logo.png" alt="logo" />
 
-				<TextField
-					required
-					label='Email'
-					value={email}
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
-					fullWidth
-				/>
+        <TextField
+          required
+          label="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          fullWidth
+        />
 
-				<TextField
-					style={{ margin: '20px 0' }}
-					required
-          type='password'
-					label='Password'
-					value={password}
-					onChange={(e) => {
-						setPassword(e.target.value);
-					}}
-					fullWidth
-				/>
+        <TextField
+          style={{ margin: "20px 0" }}
+          required
+          type="password"
+          label="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          fullWidth
+        />
 
-				<Button
-					fullWidth
-					variant='contained'
-					color='primary'
-					onClick={() => {
-						onSummit();
-					}}
-				>
-					Sign In
-				</Button>
-			</div>
-		</Container>
-	);
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            onSummit();
+          }}
+        >
+          Sign In
+        </Button>
+      </div>
+    </Container>
+  );
 });
